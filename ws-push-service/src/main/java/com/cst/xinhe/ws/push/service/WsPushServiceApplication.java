76cffa0b5d65a3a73836f1448c129a7b986c13f4
lifeng <lifeng@zkxh.com>
2019-04-26 18:29:13 +0800

    ws-push-service init

---
+++ ws-push-service/src/main/java/com/cst/xinhe/ws/push/service/WsPushServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cst.xinhe.ws.push.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPushServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsPushServiceApplication.class, args);
+    }
+
+}

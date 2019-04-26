c070acf5a48136728b8ea386a87f1307413ed427
lifeng <lifeng@zkxh.com>
2019-04-26 18:41:57 +0800

    kafka-consumer-service init

---
+++ kafka-consumer-service/src/main/java/com/cst/xinhe/kafka/consumer/service/KafkaConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cst.xinhe.kafka.consumer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerServiceApplication.class, args);
+    }
+
+}

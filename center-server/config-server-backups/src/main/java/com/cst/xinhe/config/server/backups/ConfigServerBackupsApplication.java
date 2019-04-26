cbd41ee87c565f7e2cb0aeb1599b3baeb7c9bd60
lifeng <lifeng@zkxh.com>
2019-04-26 17:14:36 +0800

    pom

--- center-server/config-server-backups/src/main/java/com/cst/xinhe/config/server/backups/ConfigServerBackupsApplication.java
+++ center-server/config-server-backups/src/main/java/com/cst/xinhe/config/server/backups/ConfigServerBackupsApplication.java
@@ -2,7 +2,12 @@ package com.cst.xinhe.config.server.backups;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerBackupsApplication {
 

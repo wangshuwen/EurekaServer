161f4414a675e1b76c10fa9b74e0ec5ecc18667e
lifeng <lifeng@zkxh.com>
2019-04-28 11:30:46 +0800

    uaa-service init

---
+++ uaa-service/src/main/java/com/cst/xinhe/uaa/service/UaaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cst.xinhe.uaa.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UaaServiceApplication.class, args);
+    }
+
+}

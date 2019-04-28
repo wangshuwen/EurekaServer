48de41030664fec39b144ef4e8d8f8331333d164
lifeng <lifeng@zkxh.com>
2019-04-28 17:59:05 +0800

    service init

---
+++ chat-message-service/src/main/java/com/cst/xinhe/chatmessage/service/ChatMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cst.xinhe.chatmessage.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatMessageServiceApplication.class, args);
+    }
+
+}
---
+++ gas-service/src/main/java/com/cst/xinhe/gas/service/GasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cst.xinhe.gas.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasServiceApplication.class, args);
+    }
+
+}

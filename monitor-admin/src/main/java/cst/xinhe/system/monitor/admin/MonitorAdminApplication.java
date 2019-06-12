14c60ae279b641119b7ba492a48a9ab62466ae33
lifeng <lifeng@zkxh.com>
2019-06-12 11:37:45 +0800

    admin

---
+++ monitor-admin/src/main/java/cst/xinhe/system/monitor/admin/MonitorAdminApplication.java
@@ -0,0 +1,13 @@
+package cst.xinhe.system.monitor.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorAdminApplication.class, args);
+    }
+
+}
---
+++ monitor-admin/src/test/java/cst/xinhe/system/monitor/admin/MonitorAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package cst.xinhe.system.monitor.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonitorAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

f178d6a0de81a0a745b080da0d93950739afc2ca
shuwen <shuwen_144@163.com>
2019-06-12 10:15:45 +0800

    kafkaConsumer

---
+++ station-partition-service/src/main/java/com/cst/xinhe/stationpartition/service/client/callback/KafkaConsumerServiceClientFallback.java
@@ -0,0 +1,24 @@
+package com.cst.xinhe.stationpartition.service.client.callback;
+
+import com.cst.xinhe.base.enums.ResultEnum;
+import com.cst.xinhe.base.result.ResultUtil;
+import com.cst.xinhe.stationpartition.service.client.KafkaConsumerServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangshuwen
+ * @Description:
+ * @Date 2019/6/12/9:59
+ */
+@Component
+public class KafkaConsumerServiceClientFallback implements KafkaConsumerServiceClient{
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @Override
+    public void overmanedAlarm(Integer type, Integer staffId) {
+        logger.error(ResultUtil.jsonToStringError(ResultEnum.CALL_REMOTE_SERVER_FAIL));
+    }
+}

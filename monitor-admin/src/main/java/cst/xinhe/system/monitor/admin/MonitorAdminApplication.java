bab01fe13f06e004a6ed316f10a7aab680834c7e
lifeng <lifeng@zkxh.com>
2019-06-14 09:46:39 +0800

    email

--- monitor-admin/src/main/java/cst/xinhe/system/monitor/admin/MonitorAdminApplication.java
+++ monitor-admin/src/main/java/cst/xinhe/system/monitor/admin/MonitorAdminApplication.java
@@ -3,10 +3,13 @@ package cst.xinhe.system.monitor.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAdminServer
 public class MonitorAdminApplication {
 
